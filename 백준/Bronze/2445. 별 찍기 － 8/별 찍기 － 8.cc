ede99e910db95af54338ad52166818fc77c2886e
YooJaeJun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-15 23:44:43 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i; j++) {
+			printf("*");
+		}
+		for (int j = 1; j <= (n - i) * 2; j++) {
+			printf(" ");
+		}
+		for (int j = 1; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (int i = 1; i <= n - 1; i++) {
+		for (int j = 1; j <= n - i; j++) {
+			printf("*");
+		}
+		for (int j = 1; j <= i * 2; j++) {
+			printf(" ");
+		}
+		for (int j = 1; j <= n - i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
