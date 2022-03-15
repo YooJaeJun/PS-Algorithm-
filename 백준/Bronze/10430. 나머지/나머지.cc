e5f254d4d29e81d6e828415acd0edeec7713ddb6
YooJaeJun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-15 23:32:21 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main() {
+	int A, B, C;
+	cin >> A >> B >> C;
+	cout << (A + B) % C << endl;
+	cout << ((A % C) + (B % C)) % C << endl;
+	cout << (A * B) % C << endl;
+	cout << ((A % C) * (B % C)) % C << endl;
+	return 0;
+}
