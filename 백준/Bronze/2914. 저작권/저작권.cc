2e382f5e3cf5e10c98482ab0ca1e82f2c279fe0c
YooJaeJun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-15 23:30:37 +0900

    [Bronze V] Title: 저작권, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int a, i;
+	cin >> a >> i;
+	cout << a * (i - 1) + 1;
+
+	return 0;
+}
