5af766b0b65053d98df62afc8201b98c2a9571d7
YooJaeJun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-15 23:33:30 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << endl << a - b << endl << a * b << endl << a / b << endl << a % b << endl;
+	return 0;
+}
