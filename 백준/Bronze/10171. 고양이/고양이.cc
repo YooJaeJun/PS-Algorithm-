6a007c819f7b70f3cf5fb902a4f4c23fb7be061f
YooJaeJun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-15 23:31:54 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "\\    /\\" << endl
+		<< " )  ( ')" << endl
+		<< "(  /  )" << endl
+		<< " \\(__)|";
+	return 0;
+}
