c7ba43511cc952061fbd6db96e9df6c656218d97
YooJaeJun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-16 00:28:29 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+void solution() {
+	int n;
+	cin >> n;
+	priority_queue<int> pq;
+	for (int i = 0; i != n; i++) {
+		int x;
+		cin >> x;
+		if (x == 0) {
+			if (pq.empty()) {
+				cout << 0 << '\n';
+			}
+			else {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(x);
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	solution();
+	return 0;
+}
