70ca84853dbb4a870977cdcb66c9427e62d0db19
YooJaeJun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-15 23:45:24 +0900

    [Bronze III] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= 9; i++) {
+		printf("%d * %d = %d\n", n, i, n*i);
+	}
+	return 0;
+}
