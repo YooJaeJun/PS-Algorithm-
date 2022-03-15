dea03d0a2b4d09e868690070e483e4849f4e566c
YooJaeJun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-15 23:37:56 +0900

    [Bronze IV] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a * (b % 10) << endl;
+	cout << a * ((b % 100 - b % 10) / 10) << endl;
+	cout << a * (b / 100) << endl;
+	cout << a * b << endl;
+	return 0;
+}
