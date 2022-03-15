5d941c54fa9da8bc3b74ffce1b42fa104c6ea496
YooJaeJun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-15 23:45:50 +0900

    [Bronze III] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		printf("%d\n", i);
+	}
+	return 0;
+}
