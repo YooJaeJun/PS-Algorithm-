d89d8af9753efa5f589d4c98378f369744b89f71
YooJaeJun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-15 23:44:56 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i - 1; j++) {
+			printf(" ");
+		}
+		for (int j = 1; j <= (n - i) * 2 + 1; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (int i = 1; i <= n - 1; i++) {
+		for (int j = 1; j <= n - i - 1; j++) {
+			printf(" ");
+		}
+		for (int j = 1; j <= i * 2 + 1; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
