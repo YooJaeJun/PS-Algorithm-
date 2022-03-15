a2b628b35e33d8a29908a8c0ea532093c7f37912
YooJaeJun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-15 23:32:09 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "|\\_/|" << endl
+		<< "|q p|   /}" << endl
+		<< "( 0 )\"\"\"\\" << endl
+		<< "|\"^\"`    |" << endl
+		<< "||_/=\\\\__|";
+	return 0;
+}
