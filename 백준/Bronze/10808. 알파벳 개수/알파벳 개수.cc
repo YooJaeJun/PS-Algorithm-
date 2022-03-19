a5a367527a77555a885a176a4eb0eda1f9ce66f4
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-19 18:00:10 +0900

    [Bronze II] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solution() {
+	string s;
+	cin >> s;
+	vector<int> v(26);
+	for (auto& ch : s) {
+		v[ch - 'a']++;
+	}
+	for (auto& elem : v) {
+		cout << elem << ' ';
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	solution();
+	return 0;
+}
