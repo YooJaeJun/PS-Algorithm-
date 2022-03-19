5473b34119698e9a10e5d72cf760b11e19da3196
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-19 18:01:01 +0900

    [Bronze II] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solution() {
+	string s;
+	cin >> s;
+	cout << s.size();
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	solution();
+	return 0;
+}
