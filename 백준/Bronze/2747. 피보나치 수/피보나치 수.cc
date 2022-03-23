77f4c3ecccc5e7c8f01f836ec50e47bfd6f31922
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-23 22:02:51 +0900

    [Bronze III] Title: 피보나치 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solution() {
+	int n;
+	cin >> n;
+	int fibo[46] = {0,};
+
+	fibo[0] = 0;
+	fibo[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		fibo[i] = fibo[i - 1] + fibo[i - 2];
+	}
+	cout << fibo[n];
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	solution();
+	return 0;
+}
