7c76920088e20618d455ddd8e1b48e577abdcd67
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-25 22:42:57 +0900

    [Silver I] Title: 가장 큰 감소 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17216. 가장 큰 감소 부분 수열/가장 큰 감소 부분 수열.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void solution() {
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	vector<int> sums(n);
+	for (int i = 0; i != n; i++) {
+		cin >> v[i];
+		sums[i] = v[i];
+	}
+
+	for (int i = 0; i != n; i++) {
+		for (int j = 0; j != i; j++) {
+			if (v[i] < v[j]) {
+				sums[i] = max(sums[i], sums[j] + v[i]);
+			}
+		}
+	}
+
+	cout << *max_element(sums.begin(), sums.end()) << '\n';
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	solution();
+	return 0;
+}
