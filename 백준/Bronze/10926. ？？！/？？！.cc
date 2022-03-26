b8ee000c0bfd626cbf898519cf576868c593699c
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-26 11:31:12 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solution() {
+	string s;
+	cin >> s;
+	s += "??!";
+	cout << s;
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	solution();
+	return 0;
+}
