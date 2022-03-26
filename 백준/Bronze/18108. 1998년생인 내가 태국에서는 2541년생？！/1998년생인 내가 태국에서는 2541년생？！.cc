322f1a2b901add03f59f230dd3d4747e01c7f839
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-26 11:33:53 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solution() {
+	int y;
+	cin >> y;
+	cout << y - (2541 - 1998);
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	solution();
+	return 0;
+}
