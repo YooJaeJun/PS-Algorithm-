b2292d3602f010d321f90c22e6de0a3ca05b08d8
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-04-01 08:00:57 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 6004 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solution() {
+	string s1, s2;
+	cin >> s1 >> s2;
+	vector<vector<int>> dp(s1.size() + 1, vector<int>(s2.size() + 1));
+
+	for (int i = 1; i <= s1.size(); i++) {
+		for (int j = 1; j <= s2.size(); j++) {
+			if (s1[i - 1] == s2[j - 1]) {
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			}
+			else {
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+			}
+		}
+	}
+
+	cout << dp[s1.size()][s2.size()];
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int testCaseNum = 1;
+	//cin >> testCaseNum;
+	for (int i = 0; i != testCaseNum; i++) { solution(); }
+	return 0;
+}
