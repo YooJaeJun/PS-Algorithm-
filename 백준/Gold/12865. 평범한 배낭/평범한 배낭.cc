c527f55fb93186429dbe4f9e88eb3d3748d14ea8
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-04-05 16:00:43 +0900

    [Gold V] Title: 평범한 배낭, Time: 44 ms, Memory: 42004 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solution() {
+	int n, k;
+	cin >> n >> k;
+	vector<vector<int>> dp(n + 1, vector<int>(k + 1));
+	vector<int> w(n + 1);
+	vector<int> v(n + 1);
+	for (int i = 1; i <= n; i++) {
+		cin >> w[i] >> v[i];
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= k; j++) {
+			if (j - w[i] >= 0) {
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+			}
+			else {
+				dp[i][j] = dp[i - 1][j];
+			}
+		}
+	}
+
+	cout << dp[n][k];
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int testCaseNum = 1;
+	// cin >> testCaseNum;
+	for (int i = 0; i != testCaseNum; i++) { solution(); }
+	return 0;
+}
