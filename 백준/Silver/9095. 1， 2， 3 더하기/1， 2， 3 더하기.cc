24ec8ddd99ae3217020261a7466beb716847d860
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-04-18 21:16:03 +0900

    Update 1， 2， 3 더하기.cc

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -5,22 +5,24 @@ using namespace std;
 void solution() {
 	int t;
 	cin >> t;
+	int dp[11] = {0, };
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
+	for (int i = 4; i != 11; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+	}
 	while (t--) {
-		int n;
-		cin >> n;
-		int dp[1001] = {0, };
-		dp[0] = 1;
-		dp[1] = 1;
-		dp[2] = 2;
-		for (int i = 3; i <= n; i++) {
-			dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-		}
-		cout << dp[n] << '\n';
+		int num;
+		cin >> num;
+		cout << dp[num] << '\n';
 	}
 }
 
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	solution();
+	int testCaseNum = 1;
+	// cin >> testCaseNum;
+	for (int i = 0; i != testCaseNum; i++) { solution(); }
 	return 0;
 }
