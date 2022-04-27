8a92cd2913c18207097e50dfafcaa42af01fd936
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-04-27 13:01:20 +0900

    [Gold IV] Title: 네트워크 연결, Time: 32 ms, Memory: 4456 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> parent;
+
+int getParent(int n) {
+	if (parent[n] == n) return n;
+	return parent[n] = getParent(parent[n]);
+}
+
+void unionParent(int n1, int n2) {
+	n1 = getParent(n1);
+	n2 = getParent(n2);
+	if (n1 < n2) parent[n2] = n1;
+	else parent[n1] = n2;
+}
+
+bool findParent(int n1, int n2) {
+	n1 = getParent(n1);
+	n2 = getParent(n2);
+	return n1 == n2;
+}
+
+struct edge {
+	int n1, n2, c;
+	edge(int an1, int an2, int ac) :
+		n1(an1), n2(an2), c(ac) {}
+	bool operator<(edge ed) {
+		return c < ed.c;
+	}
+};
+
+void solution() {
+	int n, m;
+	cin >> n >> m;
+
+	vector<edge> nodes;
+	for (int i = 0; i != m; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		nodes.push_back(edge(a, b, c));
+	}
+
+	sort(nodes.begin(), nodes.end());
+
+	parent = vector<int>(n + 1);
+	for (int i = 1; i <= n; i++) {
+		parent[i] = i;
+	}
+
+	int sum = 0;
+	for (int i = 0; i != nodes.size(); i++) {
+		if (false == findParent(nodes[i].n1, nodes[i].n2)) {
+			sum += nodes[i].c;
+			unionParent(nodes[i].n1, nodes[i].n2);
+		}
+	}
+	cout << sum;
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int testCaseNum = 1;
+	// cin >> testCaseNum;
+	for (int i = 0; i != testCaseNum; i++) { solution(); }
+	return 0;
+}
