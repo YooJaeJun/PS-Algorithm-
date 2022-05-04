99a14f05c9f926bd5d760b2ecfe5e82da7d5551f
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-05-04 18:45:08 +0900

    [Gold III] Title: 웜홀, Time: 24 ms, Memory: 2332 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.cc
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -1,52 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+//#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+
+struct edge {
+	int start, end, val;
+	edge(int a, int b, int c) : start(a), end(b), val(c) {}
+};
 
 int n, m, w;
-vector<vector<int>> nodes;
-int ans;
+vector<edge> nodes;
+vi dist;
 
-void floyd() {
-	for (int k = 1; k <= n; k++) {
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n; j++) {
-				nodes[i][j] = min(nodes[i][j], nodes[i][k] + nodes[k][j]);
-				if (i == j) {
-					ans = min(ans, nodes[i][j]);
-					if (ans < 0) return;
-				}
-			}
+void bellan_ford() {
+	for (int i = 0; i < n - 1; i++) {
+		for (auto& node : nodes) {
+			// if (dist[node.start] == INT_MAX) continue;
+			// 방문하지 않은 정점에 대해서도 최단거리를 갱신한다면 ==> 시작 정점이 어디더라도 정답
+			dist[node.end] = min(dist[node.end], dist[node.start] + node.val);
 		}
 	}
+	for (auto& node : nodes) {
+		// if (dist[node.start] == INT_MAX) continue;
+		// 위와 동일
+		if (dist[node.start] + node.val < dist[node.end]) {
+			cout << "YES" << '\n';
+			return;
+		}
+	}
+	cout << "NO" << '\n';
 }
 
 void solution() {
-	ans = 1e9;
 	cin >> n >> m >> w;
 
-	nodes = vector<vector<int>>(n + 1, vector<int>(n + 1, 1e9));
 	for (int i = 0; i != m; i++) {
 		int s, e, t;
 		cin >> s >> e >> t;
-		nodes[s][e] = min(nodes[s][e], t);
-		nodes[e][s] = min(nodes[e][s], t);
+		nodes.push_back(edge(s, e, t));
+		nodes.push_back(edge(e, s, t));
 	}
 	for (int i = 0; i != w; i++) {
 		int s, e, t;
 		cin >> s >> e >> t;
-		nodes[s][e] = min(nodes[s][e], -t);
+		nodes.push_back(edge(s, e, -t));
 	}
 
-	floyd();
+	// dist = vi(n + 1, 0);
+	dist = vi(n + 1, 0);
+	//dist[1] = 0;
 
-	if (ans < 0) cout << "YES" << '\n';
-	else cout << "NO" << '\n';
+	bellan_ford();
+
+	nodes.clear();
+	dist.clear();
 }
 
-int main() {
+int32_t main() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int testCaseNum = 1;
-	cin >> testCaseNum;
-	for (int i = 0; i != testCaseNum; i++) { solution(); }
+	int t = 1;
+	cin >> t;
+	for (int i = 0; i != t; i++) { solution(); }
 	return 0;
 }
