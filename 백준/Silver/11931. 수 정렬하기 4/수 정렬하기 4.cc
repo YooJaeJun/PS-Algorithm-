e8c2d75a4992c05b36f1b75d3d43fb778f18d6e4
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-05-09 17:13:39 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 280 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+
+
+void solution() {
+	int n;
+	cin >> n;
+	vi arr(n);
+	for (int i = 0; i != n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr.begin(), arr.end(), greater<int>());
+
+	for (auto& elem : arr) {
+		cout << elem << '\n';
+	}
+}
+
+int32_t main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	for (int i = 0; i != t; i++) { solution(); }
+	return 0;
+}
