8fad88199dff3a20df8e6489626437eb6adeda3a
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-05-12 10:45:15 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+
+
+void solution() {
+	cout << 
+		"       _.-;;-._" << '\n' <<
+		"\'-..-\'|   ||   |" << '\n' <<
+		"\'-..-\'|_.-;;-._|" << '\n' <<
+		"\'-..-\'|   ||   |" << '\n' <<
+		"\'-..-\'|_.-''-._| ";
+}
+
+int32_t main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	for (int i = 0; i != t; i++) { solution(); }
+	return 0;
+}
