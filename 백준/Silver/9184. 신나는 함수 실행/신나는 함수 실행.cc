09461d10de47f95b2f6a27c01cda54147e256aeb
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-05-22 12:12:46 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 8 ms, Memory: 3212 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -18,13 +18,8 @@ int w(int a, int b, int c)
 	if (a <= 0 || b <= 0 || c <= 0) return 1;
 	if (a > 20 || b > 20 || c > 20) return w(20, 20, 20);
 	if (dp[a][b][c] != 0) return dp[a][b][c];
-	if (a < b && b < c)
-	{
-		dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
-		return dp[a][b][c];
-	}
-	dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
-	return dp[a][b][c];
+	if (a < b && b < c) return dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+	return dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
 }
 
 void solution()
