33d3c17948c2debdde7362fb4d1cf7f28b67803b
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-06-01 12:54:38 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+const int maxn = 100001;
+const double mod = 1e9 + 7;
+
+
+void solution()
+{
+	cout << "고려대학교" << '\n';
+	string YonKoJeon = "응 연대야~";
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	for (int i = 0; i != t; i++) solution();
+	return 0;
+}
