df91cda9eb3822868bc1e2bcee8371d2dc2c5de3
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-06-05 11:41:42 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+const int maxn = 1e9 + 7;
+const double mod = 1e9 + 7;
+
+
+void solution()
+{
+	string s;
+	cin >> s;
+	cout << s.front() << s.back() << '\n';
+}
+
+int32_t main()
+{
+	int t = 1;
+	cin >> t;
+	for (int i = 0; i != t; i++) solution();
+	return 0;
+}
