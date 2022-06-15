7cafe7ce995c7287885add206f1744480004ce7b
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-06-15 17:27:55 +0900

    [Silver III] Title: 01타일, Time: 12 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+const int maxn = 1e9 + 7;
+const int mod = 15746;
+
+
+void solution()
+{
+	int n;
+	cin >> n;
+	vi dp(n + 1);
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; i++)
+	{
+		dp[i] = ((dp[i - 2] % mod) + (dp[i - 1] % mod)) % mod;
+	}
+	cout << dp[n];
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	for (int i = 0; i != t; i++) solution();
+	return 0;
+}
