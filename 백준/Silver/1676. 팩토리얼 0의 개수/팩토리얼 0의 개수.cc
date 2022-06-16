f66d1cb8a5f6d14584ac499856fddc88761548ba
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-06-16 14:33:57 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+const int maxn = 1e9 + 7;
+const double mod = 1e9 + 7;
+
+
+void solution()
+{
+	int n;
+	cin >> n;
+	int five = 5, fiveOrigin = five;	// 약수 중 5, 25, 125 의 개수
+	int ans = 0;
+	while (five <= n)
+	{
+		ans += n / five;
+		five *= fiveOrigin;
+	}
+	cout << ans;
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	for (int i = 0; i != t; i++) solution();
+	return 0;
+}
