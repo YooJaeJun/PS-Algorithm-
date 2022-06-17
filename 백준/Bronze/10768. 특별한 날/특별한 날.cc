5728fa845e1cc9046410c84cbec89b4db85be6cf
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-06-17 15:43:41 +0900

    [Bronze IV] Title: 특별한 날, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+const int maxn = 1e9 + 7;
+const double mod = 1e9 + 7;
+
+
+void solution()
+{
+	int month, day;
+	cin >> month >> day;
+	if (month < 2) cout << "Before";
+	else if (month > 2) cout << "After";
+	else if (day < 18) cout << "Before";
+	else if (day > 18) cout << "After";
+	else cout << "Special";
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	for (int i = 0; i != t; i++) solution();
+	return 0;
+}
