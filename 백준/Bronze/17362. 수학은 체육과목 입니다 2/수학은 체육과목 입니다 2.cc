feca8f84d4b81f9439daecca15adf1d8456bc5b7
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-06-17 16:02:10 +0900

    [Bronze IV] Title: 수학은 체육과목 입니다 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+const int maxn = 1e9 + 7;
+const double mod = 1e9 + 7;
+
+
+void solution()
+{
+	int finger[]{ 1,2,3,4,5,4,3,2 };
+	int n;
+	cin >> n;
+	cout << finger[n % 8 == 0 ? 7 : n % 8 - 1];
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	for (int i = 0; i != t; i++) solution();
+	return 0;
+}
