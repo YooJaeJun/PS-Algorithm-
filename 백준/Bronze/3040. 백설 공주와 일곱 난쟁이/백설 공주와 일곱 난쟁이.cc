7a967ed5e19f24e3391fbe2f2e9555ff6914c63e
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-06-20 03:47:24 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.cc
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.cc
@@ -1,28 +1,52 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+const int maxn = 1e9 + 7;
+const double mod = 1e9 + 7;
 
-int main() {
-	vector<int> v;
+void solution() {
+	vi arr;
 	int sum = 0;
-	for (int i = 0; i != 9; i++) {
+	forn(i, 9)
+	{
 		int num;
 		cin >> num;
-		v.push_back(num);
+		arr.push_back(num);
 		sum += num;
 	}
-	
-	for (int i = 0; i != 9; i++) {
-		for (int j = 0; j != 9; j++) {
-			if (sum - v[i] - v[j] == 100) {
-				for (int k = 0; k != 9; k++) {
-					if (k != i && k != j) {
-						cout << v[k] << endl;
+
+	forn(i, 9)
+	{
+		for (int j = i + 1; j < 9; j++)
+		{
+			if (sum - arr[i] - arr[j] == 100)
+			{
+				forn(k, 9)
+				{
+					if (k != i and k != j)
+					{
+						cout << arr[k] << '\n';
 					}
 				}
-				return 0;
+				return;
 			}
 		}
 	}
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	for (int i = 0; i != t; i++) solution();
 	return 0;
 }
