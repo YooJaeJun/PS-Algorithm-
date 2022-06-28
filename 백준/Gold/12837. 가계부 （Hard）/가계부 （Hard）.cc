3516da6c3d48afdc6c16e292075f36e76802ce5c
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-06-28 16:50:56 +0900

    [Gold I] Title: 가계부 (Hard), Time: 88 ms, Memory: 41088 KB -BaekjoonHub

---
+++ 백준/Gold/12837. 가계부 （Hard）/가계부 （Hard）.cc
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+const int maxn = 1e9 + 7;
+const double mod = 1e9 + 7;
+
+int n;
+vi tree, arr;
+
+int init(int start, int end, int node)
+{
+	if (start == end) return tree[node] = arr[start];
+	int mid = (start + end) / 2;
+	return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
+}
+
+void update(int start, int end, int node, int idx, int diff)
+{
+	if (idx < start || idx > end) return;
+	tree[node] += diff;
+	if (start == end) return;
+	int mid = (start + end) / 2;
+	update(start, mid, node * 2, idx, diff);
+	update(mid + 1, end, node * 2 + 1, idx, diff);
+}
+
+int sum(int start, int end, int node, int left, int right)
+{
+	if (left > end || right < start) return 0;
+	if (left <= start and right >= end) return tree[node];
+	int mid = (start + end) / 2;
+	return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
+}
+
+void solution()
+{
+	int q;
+	cin >> n >> q;
+	tree = vi((n + 1) * 4);
+	arr = vi(n + 1);
+
+	while (q--)
+	{
+		int command, p, q;
+		cin >> command >> p >> q;
+		if (command == 1)
+		{
+			update(1, n, 1, p, q);
+			arr[p] += q;
+		}
+		else
+		{
+			cout << sum(1, n, 1, p, q) << '\n';
+		}
+	}
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	for (int i = 0; i != t; i++) solution();
+	return 0;
+}
