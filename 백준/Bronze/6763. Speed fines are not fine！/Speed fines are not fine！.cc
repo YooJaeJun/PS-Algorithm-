6c60f55eb08075c78f09999c9139fc6865f0a527
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-06-28 11:50:53 +0900

    [Bronze IV] Title: Speed fines are not fine!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6763. Speed fines are not fine！/Speed fines are not fine！.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+const int maxn = 1e9 + 7;
+const double mod = 1e9 + 7;
+
+
+void solution()
+{
+	int limit, speed;
+	cin >> limit >> speed;
+	int n = speed - limit;
+	if (n <= 0) cout << "Congratulations, you are within the speed limit!";
+	else
+	{
+		cout << "You are speeding and your fine is $";
+		if (1 <= n and n <= 20) cout << 100;
+		else if (21 <= n and n <= 30) cout << 270;
+		else cout << 500;
+		cout << ".";
+	}
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	for (int i = 0; i != t; i++) solution();
+	return 0;
+}
