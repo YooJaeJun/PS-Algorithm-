ae93dba5a5c083cb7207f2c1d103e2b645cd9904
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-06-28 12:00:31 +0900

    [Silver V] Title: 나이순 정렬, Time: 68 ms, Memory: 7904 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -1,20 +1,42 @@
-#include <iostream>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+const int maxn = 1e9 + 7;
+const double mod = 1e9 + 7;
 
-int main() {
-	multimap<int, string> members;
+struct info
+{
+	int age;
+	string name;
+};
+
+void solution()
+{
 	int n;
 	cin >> n;
-	while (n--) {
-		int age;
-		string name;
-		cin >> age >> name;
-		members.insert({ age, name });
-	}
-	for (auto it = members.begin(); it != members.end(); it++) {
-		cout << it->first << " " << it->second << "\n";
-	}
+	vector<info> v(n);
+	forn(i, n) cin >> v[i].age >> v[i].name;
+	stable_sort(v.begin(), v.end(), [&](info a, info b) {
+		return a.age < b.age;
+	});
+	forn(i, n) cout << v[i].age << ' ' << v[i].name << '\n';
+}
 
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	for (int i = 0; i != t; i++) solution();
 	return 0;
 }
