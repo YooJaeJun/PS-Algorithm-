0c6aabcc763923c4ccd18b0dd77ab0053411634d
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-07-04 13:15:19 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+const int maxn = 1e9 + 7;
+const double mod = 1e9 + 7;
+
+
+void solution()
+{
+	int s, k, h;
+	cin >> s >> k >> h;
+	if (s + k + h >= 100) cout << "OK";
+	else
+	{
+		if (s < k and s < h) cout << "Soongsil";
+		else if (k < s and k < h) cout << "Korea";
+		else if (h < s and h < k) cout << "Hanyang";
+	}
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	for (int i = 0; i != t; i++) solution();
+	return 0;
+}
