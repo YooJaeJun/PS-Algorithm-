5ebbcc3a0d132cb4892a7896180e927b63560a0b
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-07-09 19:31:28 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+const int maxn = 1e9 + 7;
+const int mod = 1000;
+
+void solution()
+{
+	const float p = 3.141592;
+	float d1, d2;
+	cin >> d1 >> d2;
+	float round = 2 * p * d2;
+	cout << fixed;
+	cout.precision(6);
+	cout << round + 2 * d1;
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	for (int i = 0; i != t; i++) solution();
+	return 0;
+}
