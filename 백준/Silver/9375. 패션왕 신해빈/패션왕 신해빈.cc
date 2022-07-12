a34fe0f6e7d187527a8435ba39b6ebab7f7a3a2b
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-07-12 20:51:31 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define forn(i, n) for (int i=0; i<(int)n; i++)
+#define forn1(i, n) for (int i=1; i<=(int)n; i++)
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+const int maxn = 1e9 + 7;
+const double mod = 1e9 + 7;
+
+
+void solution()
+{
+	int n;
+	cin >> n;
+	map<string, int> dic;
+	string cloth, equip;
+	forn(i, n)
+	{
+		cin >> cloth >> equip;
+		dic[equip]++;
+	}
+	int ans = 1;
+	for (auto& elem : dic)
+	{
+		ans *= (elem.second + 1);
+	}
+	cout << ans - 1 << '\n';
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	cin >> t;
+	for (int i = 0; i != t; i++) solution();
+	return 0;
+}
