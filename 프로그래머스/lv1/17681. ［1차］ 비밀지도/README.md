1ada3bf744f89be8ea8c0a42fdcda1d1dc23ead8
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-07-16 00:14:44 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 4.09 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/README.md
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 비밀지도 - 17681 
 
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/17681) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681) 
 
 ### 성능 요약
 
-메모리: 4.33 MB, 시간: 0.01 ms
+메모리: 4.09 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -1,30 +1,22 @@
-#include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 using namespace std;
 
-vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
-    vector<string> answer(n);
-    for (int i = 0; i != n; i++) {
-        int a = arr1[i] | arr2[i];
-        for (int j = 0; j != n; j++) {
-            if (a % 2 == 0) {
-                answer[i] = ' ' + answer[i];
-            }
-            else {
-                answer[i] = '#' + answer[i];
-            }
-            a /= 2;
-            if (a == 1) {
-                answer[i] = '#' + answer[i];
-                if (answer[i].size() < n) {
-                    while (answer[i].size() != n) {
-                        answer[i] = ' ' + answer[i];
-                    }
-                }
-                break;
-            }
-        }
-    }
-    return answer;
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2)
+{
+	vector<string> ans;
+	for (int i = 0; i < n; i++)
+	{
+		arr1[i] = arr1[i] | arr2[i];	// or
+		string str;
+		int total = n;
+		while(total--)
+		{
+			if (arr1[i] & 1) str = "#" + str;	// &
+			else str = " " + str;
+			arr1[i] >>= 1;	// >>
+		}
+		ans.push_back(str);
+	}
+	return ans;
 }
