b211e172427d9430192b173996a158000f56c592
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-07-22 11:56:13 +0900

    [Bronze III] Title: 홀수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+const int maxn = 1e9 + 7;
+const double mod = 1e9 + 7;
+#define checkInGrid() if (nx < 0 or nx >= n or ny < 0 or ny >= m) continue;
+
+
+void solution()
+{
+	int sum = 0, minNum = maxn;
+	forn(i, 7)
+	{
+		int num;
+		cin >> num;
+		if (num & 1)
+		{
+			sum += num;
+			if (minNum > num) minNum = num;
+		}
+
+	}
+	if (sum == 0) cout << -1;
+	else cout << sum << '\n' << minNum;
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
+	return 0;
+}
