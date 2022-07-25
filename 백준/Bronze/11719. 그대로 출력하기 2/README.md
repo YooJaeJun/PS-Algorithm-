c6b8936bfc376d02bd9ddb21776dfacbd0ce763b
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-07-25 13:41:17 +0900

    [Bronze II] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11719. 그대로 출력하기 2/README.md
+++ 백준/Bronze/11719. 그대로 출력하기 2/README.md
@@ -1,10 +1,10 @@
-# [Bronze I] 그대로 출력하기 2 - 11719 
+# [Bronze II] 그대로 출력하기 2 - 11719 
 
 [문제 링크](https://www.acmicpc.net/problem/11719) 
 
 ### 성능 요약
 
-메모리: 2024 KB, 시간: 4 ms
+메모리: 2024 KB, 시간: 0 ms
 
 ### 분류
 
--- 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -1,14 +1,33 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <cstdio>
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+const int maxn = 1e9 + 7;
+const double mod = 1e9 + 7;
+#define checkInGrid() if (nx < 0 or nx >= n or ny < 0 or ny >= m) continue;
 
-int main() {
+
+void solution()
+{
 	string s;
-	while (!cin.eof()) {
-		getline(cin, s);
-		cout << s << endl;
-	}
+	while (getline(cin, s))
+		cout << s << '\n';
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
 	return 0;
 }
