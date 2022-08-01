a4a9e11a42532f085cdcf47cac2ca4bca66dd82a
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-08-01 11:57:33 +0900

    [Bronze III] Title: 나는 요리사다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2953. 나는 요리사다/나는 요리사다.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+const int maxn = 1e9 + 7;
+const double mod = 1e9 + 7;
+#define checkInGrid() if (nx < 0 or nx >= n or ny < 0 or ny >= m) continue;
+
+
+void solution()
+{
+	vi score(5);
+	int num, maxIdx = 0, maxNum = 0;
+	forn(i, 5)
+	{
+		forn(j, 4)
+		{
+			cin >> num;
+			score[i] += num;
+		}
+		if (maxNum < score[i])
+		{
+			maxNum = score[i];
+			maxIdx = i;
+		}
+	}
+	cout << maxIdx + 1 << ' ' << maxNum;
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
+	return 0;
+}
