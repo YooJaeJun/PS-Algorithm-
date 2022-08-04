bab851eadc650328766907ddd4958dcfa8e05897
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-08-04 15:39:21 +0900

    [Gold II] Title: 2048 (Easy), Time: 4 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
@@ -15,68 +15,105 @@ int dc[4] = { -1,1,0,0 };	// 좌우상하
 
 int n;
 int ans = 0;
-const int maxMoveCnt = 5;
-vi limit(maxMoveCnt + 1);	// 뎁스별 제한값
-ddb alreadyMerge;
-vvi curBoard;
+const int maxMoveDepth = 5;
+vi limit(maxMoveDepth + 1);
+enum edir { left, right, top, bottom };
+deque<int> dq;
 
-void goBack(vvi& curBoard, const int x, const int y, int nx, int ny, int i)
+void move(vvi& board, const int dir)
 {
-	nx -= dr[i];
-	ny -= dc[i];
-	if (curBoard[nx][ny] == 0)
+	// dq에다 각 줄마다 0이 아닌 거 넣고, 각 줄 처음부터 채워넣을거임. 행인지 열인지, 시작부턴지 끝부턴지 잘 생각
+	dq.clear();
+
+	int cur = 0;
+	if (dir == edir::right or dir == edir::bottom) cur = n - 1;
+
+	if (dir == edir::left)
 	{
-		curBoard[nx][ny] = curBoard[x][y];
-		curBoard[x][y] = 0;
+		forn(r, n)	//
+		{
+			forn(c, n)	//
+			{
+				if (board[r][c] == 0) continue;
+				dq.push_back(board[r][c]);	//
+				board[r][c] = 0;
+			}
+			while (false == dq.empty())
+			{
+				if (board[r][cur] == 0) board[r][cur] = dq.front();	//
+				else if (board[r][cur] == dq.front()) board[r][cur++] *= 2;	//
+				else board[r][++cur] = dq.front(); //
+				dq.pop_front();
+			}
+			cur = 0;	//
+		}
+	}
+	else if (dir == edir::right)
+	{
+		forn(r, n)	//
+		{
+			forn(c, n)	//
+			{
+				if (board[r][c] == 0) continue;
+				dq.push_front(board[r][c]);	//
+				board[r][c] = 0;
+			}
+			while (false == dq.empty())
+			{
+				if (board[r][cur] == 0) board[r][cur] = dq.front();	//
+				else if (board[r][cur] == dq.front()) board[r][cur--] *= 2;	//
+				else board[r][--cur] = dq.front(); //
+				dq.pop_front();
+			}
+			cur = n - 1;	//
+		}
+	}
+	else if (dir == edir::top)
+	{
+		forn(c, n)	//
+		{
+			forn(r, n)	//
+			{
+				if (board[r][c] == 0) continue;
+				dq.push_back(board[r][c]);	//
+				board[r][c] = 0;
+			}
+			while (false == dq.empty())
+			{
+				if (board[cur][c] == 0) board[cur][c] = dq.front();	//
+				else if (board[cur][c] == dq.front()) board[cur++][c] *= 2;	//
+				else board[++cur][c] = dq.front();	//
+				dq.pop_front();
+			}
+			cur = 0;	//
+		}
+	}
+	else if (dir == edir::bottom)
+	{
+		forn(c, n)	//
+		{
+			forn(r, n)	//
+			{
+				if (board[r][c] == 0) continue;
+				dq.push_front(board[r][c]);	//
+				board[r][c] = 0;
+			}
+			while (false == dq.empty())
+			{
+				if (board[cur][c] == 0) board[cur][c] = dq.front();	//
+				else if (board[cur][c] == dq.front()) board[cur--][c] *= 2;	//
+				else board[--cur][c] = dq.front();	//
+				dq.pop_front();
+			}
+			cur = n - 1;	//
+		}
 	}
 }
 
-bool move(vvi& board, const int dir)
+bool isSame(vvi& board, vvi& boardOrigin)
 {
-	bool moved = false;	// 안 움직였으면 dfs no
-	// 좌0 우1 상2 하3... r == row, c == column, coef == 증감용
-	int startR = 0, startC = 0, endR = n, endC = n, coefR = 1, coefC = 1;
-	if (dir == 1) startR = 0, startC = n - 1, endR = n, endC = -1, coefR = 1, coefC = -1;
-	else if (dir == 3) startR = n - 1, startC = 0, endR = -1, endC = n, coefR = -1, coefC = 1;
-
-	for (int r = startR; r != endR; r += coefR)
-	{
-		for (int c = startC; c != endC; c += coefC)
-		{
-			if (board[r][c] == 0) continue;
-			if (alreadyMerge[r][c]) continue;
-
-			int nr = r + dr[dir];
-			int nc = c + dc[dir];
-			while (1)
-			{
-				if (inGrid() == false)
-				{
-					goBack(board, r, c, nr, nc, dir);
-					break;
-				}
-				if (board[nr][nc] != 0)
-				{
-					if (alreadyMerge[nr][nc] || (board[nr][nc] != board[r][c]))
-					{
-						goBack(board, r, c, nr, nc, dir);
-					}
-					else
-					{
-						board[nr][nc] *= 2;
-						board[r][c] = 0;
-						alreadyMerge[nr][nc] = true;
-						moved = true;
-					}
-					break;
-				}
-				nr += dr[dir];
-				nc += dc[dir];
-				moved = true;
-			}
-		}
-	}
-	return moved;
+	forn(r, n) forn(c, n) if (board[r][c] != boardOrigin[r][c]) return false;
+	return true;
 }
 
 void dfs(vvi& board, int depth)
@@ -90,25 +127,24 @@ void dfs(vvi& board, int depth)
 	if (curMax <= limit[depth]) return;
 
 	// 끝까지 갔을 때 depth별 limit 설정
-	if (depth == maxMoveCnt)
+	if (depth == maxMoveDepth)
 	{
 		ans = max(ans, curMax);
-		limit[maxMoveCnt] = ans;
-		for (int i = maxMoveCnt; i > 0; i--)
+		limit[maxMoveDepth] = ans;
+		for (int i = maxMoveDepth; i > 0; i--)
 			limit[i - 1] = limit[i] / 2;
 		return;
 	}
-	
+
 	vvi boardOrigin = board;
 	forn(r, n) forn(c, n) boardOrigin[r][c] = board[r][c];
 
 	// 방향별 이동
 	forn(dir, 4)
 	{
-		forn(i, n) fill(alreadyMerge[i].begin(), alreadyMerge[i].end(), false);	// 한 번 합쳐졌었나
-
 		// 이 방향에서 움직여도 변화 없었으면 더 이상 x
-		if (move(board, dir))
+		move(board, dir);
+		if (false == isSame(board, boardOrigin))
 		{
 			dfs(board, depth + 1);
 			forn(r, n) forn(c, n) board[r][c] = boardOrigin[r][c];
@@ -119,15 +155,13 @@ void dfs(vvi& board, int depth)
 void solution() {
 	cin >> n;
 	vvi board(n, vi(n));
-	alreadyMerge = ddb(n, db(n));
-	curBoard = vvi(n, vi(n));
 
-	for (int i = 0; i != n; i++)
+	for (int r = 0; r != n; r++)
 	{
-		for (int j = 0; j != n; j++)
+		for (int c = 0; c != n; c++)
 		{
-			cin >> board[i][j];
-			ans = max(ans, board[i][j]);
+			cin >> board[r][c];
+			ans = max(ans, board[r][c]);
 		}
 	}
 
