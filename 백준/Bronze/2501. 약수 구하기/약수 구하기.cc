b8e581bb246145b820650482c7f5351ab7ed1917
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-08-07 11:26:35 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
+int dr[4] = { 0,0,-1,1 };
+int dc[4] = { -1,1,0,0 };	// 좌우상하
+
+
+void solution()
+{
+	int n, k;
+	cin >> n >> k;
+	int cnt = 0;
+	forn1(i, n)
+	{
+		if (n % i == 0) cnt++;
+		if (cnt == k)
+		{
+			cout << i;
+			return;
+		}
+	}
+	cout << 0;
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
+	return 0;
+}
