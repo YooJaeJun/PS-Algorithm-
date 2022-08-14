e7e560cc4d5fb0fb7d54ac8d9816645b790e99e9
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-08-14 12:03:46 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
+int dr[4] = { 0,0,-1,1 };
+int dc[4] = { -1,1,0,0 };	// 좌우상하
+
+void solution()
+{
+	string name;
+	int age, weight;
+	
+	while (1)
+	{
+		cin >> name >> age >> weight;
+		if (name == "#" and age == 0 and weight == 0) return;
+		cout << name << ' ';
+		if (age > 17 or weight >= 80) cout << "Senior" << '\n';
+		else cout << "Junior" << '\n';
+	}
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
+	return 0;
+}
