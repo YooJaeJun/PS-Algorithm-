a515c3d4e5542db2ca3ecfd0d1d2fdb5bf838b3d
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-08-16 17:28:22 +0900

    [Bronze IV] Title: 공백 없는 A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15873. 공백 없는 A＋B/공백 없는 A＋B.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
+int dr[4] = { 0,0,-1,1 };
+int dc[4] = { -1,1,0,0 };	// 좌우상하
+
+
+void solution()
+{
+	int n;
+	cin >> n;
+	if (n < 100) cout << n / 10 + n % 10;
+	else if (n == 1010) cout << 20;
+	else if (n / 10 == 10) cout << 10 + n % 100;
+	else cout << n / 100 + 10;
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
+	return 0;
+}
