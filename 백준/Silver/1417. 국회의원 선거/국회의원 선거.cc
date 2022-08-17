7962ebbdd70c8c9ee3b6134bb6933bdfa9dc6c9f
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-08-17 09:37:39 +0900

    [Silver V] Title: 국회의원 선거, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
+int dr[4] = { 0,0,-1,1 };
+int dc[4] = { -1,1,0,0 };	// 좌우상하
+
+
+void solution()
+{
+	int n;
+	cin >> n;
+	vi v(n);
+	forn(i, n) cin >> v[i];
+	if (n == 1)
+	{
+		cout << 0;
+		return;
+	}
+
+	int ans = 0;
+	while (1)
+	{
+		int maxIdx = max_element(v.begin() + 1, v.end()) - v.begin();
+		if (v[0] > v[maxIdx]) break;
+		v[0]++;
+		v[maxIdx]--;
+		ans++;
+	}
+	cout << ans;
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
+	return 0;
+}
