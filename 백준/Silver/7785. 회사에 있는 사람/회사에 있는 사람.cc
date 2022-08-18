ae68eee80f35f0bbd999afca7e41901c4d071b6b
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-08-18 12:52:09 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 96 ms, Memory: 9948 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
+int dr[4] = { 0,0,-1,1 };
+int dc[4] = { -1,1,0,0 };	// 좌우상하
+
+
+void solution()
+{
+	int n;
+	cin >> n;
+	string person, act;
+	map<string, int> ans;
+	while (n--)
+	{
+		cin >> person >> act;
+		if (act == "enter") ans[person]++;
+		else ans[person]--;
+	}
+	
+	for (auto it = --ans.end(); ; it--)
+	{
+		if (it->second > 0)
+		{
+			cout << it->first << '\n';
+		}
+		if (it == ans.begin()) break;
+	}
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
+	return 0;
+}
