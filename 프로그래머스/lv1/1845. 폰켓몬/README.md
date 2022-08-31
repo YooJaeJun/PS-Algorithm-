f0f8afa251f68ee56ee089c446bd412da0b1425a
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-09-01 00:49:50 +0900

    [level 1] Title: 폰켓몬, Time: 0.18 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/README.md
+++ 프로그래머스/lv1/1845. 폰켓몬/README.md
@@ -1,14 +1,14 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/1845) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
 
 ### 성능 요약
 
-메모리: 4.26 MB, 시간: 0.26 ms
+메모리: 4.19 MB, 시간: 0.18 ms
 
 ### 구분
 
-코딩테스트 연습 > 찾아라 프로그래밍 마에스터
+코딩테스트 연습 > 해시
 
 ### 채점결과
 
--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,27 +1,9 @@
-#include <vector>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
 
 int solution(vector<int> nums)
 {
-	int answer = 0;
-	map<int, int> poketmons;	//종류, 개수
-	//종류별 개수 세팅
-	for (const int elem : nums) {
-		auto it = poketmons.find(elem);
-		if (it != poketmons.end()) {
-			poketmons[elem]++;
-		}
-		else {
-			poketmons[elem] = 1;
-		}
-	}
-	//검사
-	if (poketmons.size() <= nums.size() / 2) {
-		answer = poketmons.size();
-	}
-	else {
-		answer = nums.size() / 2;
-	}
-	return answer;
+    unordered_map<int, int> dic;
+    for(auto& num : nums) dic[num]++;
+	return min(dic.size(), nums.size() / 2);
 }
