3c2693e8d7609ecf0a29a42d93ccf971ea91b775
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-09-01 11:32:46 +0900

    [Silver IV] Title: 차집합, Time: 624 ms, Memory: 33308 KB -BaekjoonHub

---
+++ 백준/Silver/1822. 차집합/차집합.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define endl '\n';
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
+int dr[4] = { 0,0,-1,1 };
+int dc[4] = { -1,1,0,0 };	// 좌우상하
+
+
+void solution()
+{
+	int na, nb;
+	cin >> na >> nb;
+	map<int, int> dic;
+	int num;
+	forn(i, na)
+	{
+		cin >> num;
+		dic[num]++;
+	}
+	forn(i, nb)
+	{
+		cin >> num;
+		dic.erase(num);
+	}
+
+	cout << dic.size() << '\n';
+	for (auto& elem : dic) cout << elem.first << ' ';
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
+	return 0;
+}
