95041794375ed839878f7b2c657a686c44712c1c
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-09-20 23:40:29 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
+int dr[4] = { 0,0,-1,1 };
+int dc[4] = { -1,1,0,0 };	// 좌우상하
+
+
+void solution()
+{
+	int n;
+	cin >> n;
+	vi v(n + 1, -1);
+	int idx, loc, ans = 0;
+	forn(i, n)
+	{
+		cin >> idx >> loc;
+		if (v[idx] == -1)
+		{
+			v[idx] = loc;
+		}
+		else if (v[idx] != loc)
+		{
+			v[idx] = loc;
+			ans++;
+		}
+	}
+	cout << ans;
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
+	return 0;
+}
