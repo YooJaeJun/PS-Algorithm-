1049ee3bcc093bb18c54fcd70c2278c70922e7d1
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-10-03 12:56:27 +0900

    [Bronze I] Title: 행복한지 슬픈지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10769. 행복한지 슬픈지/행복한지 슬픈지.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
+int dr[4] = { 0,0,-1,1 };
+int dc[4] = { -1,1,0,0 };	// 좌우상하
+
+
+void solution()
+{
+	string s;
+	getline(cin, s);
+
+	int happy = 0, sad = 0;
+	forn(i, s.size())
+	{
+		if (s[i] == ':')
+		{
+			string s2 = s.substr(i, 3);
+			if (s2 == ":-)") happy++;
+			else if (s2 == ":-(") sad++;
+		}
+	}
+
+	if (happy > sad) cout << "happy";
+	else if (happy < sad) cout << "sad";
+	else if (happy == 0 && sad == 0) cout << "none";
+	else cout << "unsure";
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
+	return 0;
+}
