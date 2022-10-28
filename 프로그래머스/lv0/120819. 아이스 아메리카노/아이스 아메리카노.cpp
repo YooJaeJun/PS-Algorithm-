e917e21ed080ae614be0ffb4b45592cdb5636fab
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-10-28 16:11:45 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 4.08 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int money) {
+    vector<int> answer;
+    answer.push_back(money / 5500);
+    answer.push_back(money % 5500);
+    return answer;
+}
