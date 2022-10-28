f652b22d1b6f697c3876fb8503763ce439dfa5a2
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-10-28 21:27:43 +0900

    [level 0] Title: A로 B 만들기, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(string before, string after) {
+    sort(before.begin(), before.end());
+    sort(after.begin(), after.end());
+    return before == after;
+}
