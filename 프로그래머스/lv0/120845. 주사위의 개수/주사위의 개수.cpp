c511e46d0c669c563867f8b3f5d7fe0066569fa3
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-10-28 19:52:44 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> box, int n) {
+    int answer = 1;
+    for (int i=0; i<3; i++)
+    {
+        answer *= box[i] / n;
+    }
+    return answer;
+}
