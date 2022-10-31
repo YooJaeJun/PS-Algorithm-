7478c2c7118b46f40723b0e6cee4b206f2be04d9
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-10-31 14:09:07 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 3.47 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cpp
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cpp
@@ -5,12 +5,15 @@
 using namespace std;
 
 int solution(int balls, int share) {
+    vector<vector<int>> dp(balls + 1, vector<int>(balls + 1));
+    
     function<int(int, int)> combi = [&](int n, int r)
     {
         if (r == 0) return 1;
         else if (n == 1) return 1;
         else if (r >= n) return 1;
-        return combi(n-1, r) + combi(n-1, r-1);
+        else if (dp[n][r]) return dp[n][r];
+        return dp[n][r] = combi(n-1, r) + combi(n-1, r-1);
     };
     return combi(balls, share);
 }
