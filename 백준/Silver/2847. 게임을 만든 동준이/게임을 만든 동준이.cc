5d9a087af88e85a9dfbfbd3669c1c6918cedf97c
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-11-04 10:44:21 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
+int dr[4] = { 0,0,-1,1 };
+int dc[4] = { -1,1,0,0 };	// 좌우상하
+
+
+void solution()
+{
+	int n;
+	cin >> n;
+	
+	vi v(n);
+	forn(i, n) cin >> v[i];
+
+	int sum = 0;
+	for (int i = v.size() - 1; i >= 1; i--)
+	{
+		if (v[i] <= v[i - 1])
+		{
+			sum += v[i - 1] - (v[i] - 1);
+			v[i - 1] = v[i] - 1;
+		}
+	}
+
+	cout << sum;
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
+	return 0;
+}
