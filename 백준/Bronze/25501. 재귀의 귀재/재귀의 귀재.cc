45002cd56bc08e8eb786e61e80990a6c30280b20
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-11-08 16:48:15 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
+int dr[4] = { 0,0,-1,1 };
+int dc[4] = { -1,1,0,0 };	// 좌우상하
+
+
+void solution()
+{
+	int n;
+	cin >> n;
+	forn(i, n)
+	{
+		string s;
+		cin >> s;
+
+		int ans = 0;
+		bool flag = true;
+		for (int i = 0; i < s.size() / 2; i++)
+		{
+			if (s[i] == s[s.size() - 1 - i]) ans++;
+			else
+			{
+				flag = false;
+				break;
+			}
+		}
+		cout << flag << ' ' << ans + 1 << '\n';
+	}
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
+	return 0;
+}
