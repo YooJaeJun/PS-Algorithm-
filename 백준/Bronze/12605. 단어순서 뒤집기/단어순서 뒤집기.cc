306b76d5ce0dd1ab85bfd524c7cdf489f30204b8
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-11-09 13:12:06 +0900

    [Bronze I] Title: 단어순서 뒤집기, Time: 0 ms, Memory: 2080 KB -BaekjoonHub

---
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
+int dr[4] = { 0,0,-1,1 };
+int dc[4] = { -1,1,0,0 };	// 좌우상하
+
+
+void solution()
+{
+	int n;
+	cin >> n;
+	stringstream ss;
+	string s;
+	cin.ignore();
+
+	forn(i, n)
+	{
+		getline(cin, s);
+		ss.str(s);
+
+		stack<string> st;
+		string temp;
+		while (ss >> temp)
+		{
+			st.push(temp);
+		}
+
+		cout << "Case #" << i + 1 << ": ";
+		while (!st.empty())
+		{
+			cout << st.top() << ' ';
+			st.pop();
+		}
+		cout << '\n';
+
+		ss.clear();
+	}
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
+	return 0;
+}
