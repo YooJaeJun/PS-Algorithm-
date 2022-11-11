c6912f74b0352a8385fa6c97fb1c98fe0a8ea999
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-11-11 19:25:24 +0900

    [Bronze III] Title: 전투 드로이드 가격, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5361. 전투 드로이드 가격/전투 드로이드 가격.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
+int dr[4] = { 0,0,-1,1 };
+int dc[4] = { -1,1,0,0 };	// 좌우상하
+
+
+void solution()
+{
+	int t;
+	cin >> t;
+	vector<double> price{ 350.34, 230.90, 190.55, 125.30, 180.90 };
+	while (t--)
+	{
+		double sum = 0;
+		forn(i, 5)
+		{
+			double num;
+			cin >> num;
+			sum += price[i] * num;
+		}
+		cout << fixed;
+		cout.precision(2);
+		cout << '$' << sum << '\n';
+	}
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
+	return 0;
+}
