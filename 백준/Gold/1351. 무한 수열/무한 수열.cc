6deac76c09d59ec96e29a7a5df0262af38d959d4
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-11-17 15:43:36 +0900

    [Gold V] Title: 무한 수열, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Gold/1351. 무한 수열/무한 수열.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
+int dr[4] = { 0,0,-1,1 };
+int dc[4] = { -1,1,0,0 };	// 좌우상하
+
+
+void solution()
+{
+	int n, p, q;
+	cin >> n >> p >> q;
+	unordered_map<int, int> dic;
+	dic[0] = 1;
+	dic[1] = 2;
+
+	function<int(int)> recur = [&](int i)
+	{
+		if (dic[i]) return dic[i];
+		return dic[i] = recur(i / p) + recur(i / q);
+	};
+	recur(n);
+
+	cout << dic[n];
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
+	return 0;
+}
