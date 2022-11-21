6651c37e4416e7954ef5b23af879bbe9eefe7225
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-11-21 16:54:27 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2392 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
+int dr[4] = { 0,0,-1,1 };
+int dc[4] = { -1,1,0,0 };	// 좌우상하
+
+
+void solution()
+{
+	int n;
+	cin >> n;
+	int ans = 0;
+
+	forn(i, n)
+	{
+		string s;
+		cin >> s;
+		stack<char> st;
+
+		for (auto& ch : s)
+		{
+			if (st.empty()) st.push(ch);			
+			else if ((st.top() == 'A' and ch == 'A') or
+				(st.top() == 'B' and ch == 'B'))
+			{
+				st.pop();
+			}
+			else st.push(ch);
+		}
+		
+		if (st.empty()) ans++;
+	}
+	cout << ans;
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
+	return 0;
+}
