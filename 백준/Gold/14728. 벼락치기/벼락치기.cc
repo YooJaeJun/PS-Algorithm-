69917204729b0f59d0e37b7f0dce3455b689f326
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-11-22 12:57:39 +0900

    [Gold V] Title: 벼락치기, Time: 8 ms, Memory: 10036 KB -BaekjoonHub

---
+++ 백준/Gold/14728. 벼락치기/벼락치기.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
+int dr[4] = { 0,0,-1,1 };
+int dc[4] = { -1,1,0,0 };	// 좌우상하
+
+
+void solution()
+{
+	int n, t;
+	cin >> n >> t;
+	vi w(n + 1), v(n + 1);
+	forn1(i, n)
+	{
+		cin >> w[i] >> v[i];
+	}
+	vvi dp(n + 1, vi(t + 1));
+	
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= t; j++)
+		{
+			if (j < w[i]) dp[i][j] = dp[i - 1][j];
+			else dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+		}
+	}
+
+	cout << dp[n][t];
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--) solution();
+	return 0;
+}
