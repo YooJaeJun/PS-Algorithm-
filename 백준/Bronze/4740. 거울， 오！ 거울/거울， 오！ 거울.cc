4f6880054b97d0a1415287ebf3e89f5214721b16
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-12-07 11:22:51 +0900

    [Bronze III] Title: 거울, 오! 거울, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4740. 거울， 오！ 거울/거울， 오！ 거울.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using db = deque<bool>;
+using ddb = deque<deque<bool>>;
+#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
+#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
+int dr[4] = { 0,0,-1,1 };
+int dc[4] = { -1,1,0,0 };	// 좌우상하
+
+
+void solution()
+{
+	while (true)
+	{
+		string s;
+		getline(cin, s);
+		if (s == "***") break;
+
+		reverse(s.begin(), s.end());
+		cout << s << '\n';
+	}
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solution();
+	return 0;
+}
