f2c67c6783cb593b8e2fa39a0ee9bbcef62ca512
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-04-14 22:54:48 +0900

    [Gold V] Title: 최소비용 구하기, Time: 40 ms, Memory: 4228 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -1,62 +1,60 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <stack>
 using namespace std;
 
-int n, m;
-vector<vector<pair<int, int>>> nodes;
-vector<int> dist;
-vector<bool> visited;
-const int inf = 1e9;
-int ans = inf;
-
-void dijk(int start) {
-	priority_queue<pair<int, int>> pq;	// 거리, 연결노드
-	pq.push({ 0, start });
-
-	while (pq.empty() == false) {
-		int distance = pq.top().first;
-		int cur = pq.top().second;
-		pq.pop();
-
-		if (dist[cur] < distance) continue;
-
-		for (int i = 0; i != nodes[cur].size(); i++) {
-			int nDistance = distance + nodes[cur][i].first;
-			int nCur = nodes[cur][i].second;
-
-			if (dist[nCur] > nDistance) {
-				dist[nCur] = nDistance;
-				pq.push({ nDistance, nCur });
-			}
-		}
-	}
-}
-
-void solution() {
+void Solution()
+{
+	int n, m;
 	cin >> n >> m;
-	nodes.resize(n + 1);
-	visited.resize(n + 1);
-	dist.resize(n + 1, inf);
 
-	for (int i = 0; i != m; i++) {
-		int n1, n2, d;
-		cin >> n1 >> n2 >> d;
-		nodes[n1].push_back({ d, n2 });
+	vector<vector<pair<int, int>>> adj(n + 1);
+
+	for (int i = 0; i < m; i++)
+	{
+		int n1, n2, dist;
+		cin >> n1 >> n2 >> dist;
+		adj[n1].push_back({ dist, n2 });
 	}
 
 	int start, end;
 	cin >> start >> end;
-	dijk(start);
+	vector<int> dist(n + 1, 1e9);
+
+	auto Dijk = [&]()
+	{
+		priority_queue<pair<int, int>> pq;
+		pq.push({ 0, start });
+
+		while(pq.empty() == false)
+		{
+			int curDist = pq.top().first;
+			int cur = pq.top().second;
+			pq.pop();
+
+			if (dist[cur] < curDist)
+				continue;
+
+			for (auto& elem : adj[cur])
+			{
+				int nextDist = curDist + elem.first;
+				int next = elem.second;
+
+				if (dist[next] > nextDist)
+				{
+					dist[next] = nextDist;
+					pq.push({ nextDist, next });
+				}
+			}
+		}
+	};
+	Dijk();
 
 	cout << dist[end];
 }
 
-int main() {
+int main()
+{
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int testCaseNum = 1;
-	// cin >> testCaseNum;
-	for (int i = 0; i != testCaseNum; i++) { solution(); }
-	return 0;
+	Solution();
 }
