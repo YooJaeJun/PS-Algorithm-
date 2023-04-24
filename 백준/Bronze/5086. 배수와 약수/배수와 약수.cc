c6e536dd30a98ca779c70ab178233c04febe2fad
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-04-24 12:39:32 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -1,37 +1,26 @@
 #include <iostream>
-#include <vector>
+
 using namespace std;
 
-bool isFactor(int n1, int n2) {
-	return n2 % n1 == 0;
-}
+int main()
+{
+	enum { factor = 0, multiple = 1, neither = 2 };
+	string ans[3] = { "factor", "multiple", "neither" };
 
-bool isMultiple(int n1, int n2) {
-	return n1 % n2 == 0;
-}
+	int a = 0, b = 0;
 
-void solution() {
-	while (1) {
-		int n1, n2;
-		cin >> n1 >> n2;
-		if (n1 == 0 && n2 == 0) return;
+	while (cin >> a >> b)
+	{
+		if (a == 0 && b == 0)
+			break;
 
-		if (isFactor(n1, n2)) {
-			cout << "factor" << '\n';
-		}
-		else if (isMultiple(n1, n2)) {
-			cout << "multiple" << '\n';
-		}
-		else {
-			cout << "neither" << '\n';
-		}
+		if (a < b && b % a == 0)
+			cout << ans[factor] << '\n';
+		else if (b < a && a % b == 0)
+			cout << ans[multiple] << '\n';
+		else
+			cout << ans[neither] << '\n';
 	}
-}
 
-int main() {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int testCaseNum = 1;
-	// cin >> testCaseNum;
-	for (int i = 0; i != testCaseNum; i++) { solution(); }
 	return 0;
 }
