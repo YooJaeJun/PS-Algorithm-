8de3fd168db4ffe056b3c19110588e4f8940b744
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-04-25 11:51:43 +0900

    [Bronze I] Title: 약수들의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.cc
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.cc
@@ -1,75 +1,42 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
-#define int int64_t
-using ll = long long;
-using pii = pair<int, int>;
-using vi = vector<int>;
-using vvi = vector<vector<int>>;
-using db = deque<bool>;
-using ddb = deque<deque<bool>>;
-#define forn(i, n) for (int i = 0; i < (int)n; i++)
-#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
-#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
-int dr[4] = { 0,0,-1,1 };
-int dc[4] = { -1,1,0,0 };	// 좌우상하
 
-
-void solution()
+int main()
 {
-	int n;
 	while (1)
 	{
+		int n;
 		cin >> n;
-		if (n == -1) break;
-		float root = sqrt(n);
-		set<int> remains{ 1 };
+		if (n == -1)
+			return 0;
 
-		for (int i = 2; i < root; i++)
-		{
+		vector<int> v;
+
+		for (int i = 1; i < n; i++)
 			if (n % i == 0)
-			{
-				remains.insert(i);
-				remains.insert(n / i);
-			}
-		}
-		if ((int)root * root == n)
-		{
-			remains.insert(root);
-		}
+				v.push_back(i);
 
 		int sum = 0;
-		for (auto& elem : remains)
-		{
+
+		for (auto& elem : v)
 			sum += elem;
-		}
 
 		if (sum != n)
-		{
 			cout << n << " is NOT perfect.\n";
-		}
 		else
 		{
 			cout << n << " = ";
-			
-			int idx = 0;
-			for (auto& elem : remains)
+			for (auto& elem : v)
 			{
-				if (idx < remains.size() - 1)
-				{
-					cout << elem << " + ";
-				}
-				else cout << elem << '\n';
-				idx++;
+				cout << elem;
+				if (elem != v.back())
+					cout << " + ";
 			}
+			cout << '\n';
 		}
 	}
-}
 
-int32_t main()
-{
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int t = 1;
-	// cin >> t;
-	while (t--) solution();
 	return 0;
 }
