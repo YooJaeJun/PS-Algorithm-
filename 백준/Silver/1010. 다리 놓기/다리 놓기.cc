9b72566b2796f8fc76d0ff4864bd64b61a2a8bd7
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-04-27 12:43:39 +0900

    [Silver V] Title: 다리 놓기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.cc
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -1,30 +1,25 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <vector>
 using namespace std;
 
-#define MAXINT 30
-int pas[MAXINT + 1][MAXINT + 1];
-
-void bridge() {
-	//조합: nCk = nPk/k! = n!/(k!(n-k)!) 으론 오버플로
-	//파스칼 삼각형: 행렬 (n, k) =  (n - 1, k - 1) + (n - 1, k)
-	pas[0][0] = 1;
-	for (int i = 1; i <= MAXINT; i++) {
-		pas[i][0] = 1;
-		for (int j = 1; j <= i; j++) {
-			pas[i][j] = pas[i - 1][j - 1] + pas[i - 1][j];
-		}
-	}
-}
-int main() {
-	bridge();	//파스칼 삼각형 세팅
+int main()
+{
 	int t;
 	cin >> t;
-	int n1, n2;
-	for (int i = 0; i != t; i++) {
-		cin >> n1 >> n2;
-		printf("%d\n", pas[n2][n1]);
+
+	while (t--)
+	{
+		int n, m;
+		cin >> n >> m;
+		long long ans = 1;
+
+		for (int i = 0; i < n; i++)
+		{
+			ans *= m - i;
+			ans /= i + 1;
+		}
+
+		cout << ans << '\n';
 	}
+
 	return 0;
 }
