8443c0114afe5aa27192f73ae37217f9e995d636
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-05-01 02:21:31 +0900

    [Silver IV] Title: 가로수, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.cc
+++ 백준/Silver/2485. 가로수/가로수.cc
@@ -1,49 +1,44 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <functional>
 using namespace std;
-#define int int64_t
-using ll = long long;
-using pii = pair<int, int>;
-using vi = vector<int>;
-using vvi = vector<vector<int>>;
-using db = deque<bool>;
-using ddb = deque<deque<bool>>;
-#define forn(i, n) for (int i = 0; i < (int)n; i++)
-#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
-#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
-int dr[4] = { 0,0,-1,1 };
-int dc[4] = { -1,1,0,0 };	// 좌우상하
 
-
-void solution()
-{
-	int n;
-	cin >> n;
-	vi v(n);
-
-	function<int(int, int)> gcd = [&](int a, int b)
-	{
-		if (b == 0) return a;
-		return gcd(b, a % b);
-	};
-
-	forn(i, n) cin >> v[i];
-
-	int res = v[1] - v[0];
-	for (int i = 2; i < n; i++)
-	{
-		res = gcd(v[i] - v[i - 1], res);
-	}
-
-	int ans = (v[n - 1] - v[0]) / res + 1;
-	ans -= n;
-	cout << ans;
-}
-
-int32_t main()
+int main()
 {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int t = 1;
-	// cin >> t;
-	while (t--) solution();
+
+	int n;
+	cin >> n;
+
+	function<int(const int, const int)> Gcd = [&](const int a, const int b)
+	{
+		if (b == 0) return a;
+		return Gcd(b, a % b);
+	};
+
+	vector<int> v(n);
+	int prev = -1, input = 0, gcd = -1;
+
+	cin >> input;
+	v[0] = prev = input;
+
+	for (int i=1; i<n; i++)
+	{
+		cin >> input;
+		v[i] = input;
+		if (i == 1)
+			gcd = input - prev;
+		else 
+			gcd = Gcd(gcd, input - prev);
+		prev = input;
+	}
+
+	int ans = 0;
+
+	for (int i=1; i<n; i++)
+		ans += (v[i] - v[i - 1]) / gcd - 1;
+
+	cout << ans;
+
 	return 0;
 }
