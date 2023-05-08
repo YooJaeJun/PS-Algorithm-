d0942698a299df57899df2e9c2dc6bd00f614568
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-05-08 15:16:32 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2304 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,64 +1,72 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <algorithm>
+#include <deque>
+#include <queue>
+#include <functional>
+
 using namespace std;
 
-vector<int> visited;
-vector<vector<int>> nodes;
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-void initVisited() {
-	for (auto& elem : visited) {
-		elem = 0;
+	int n, m, v;
+	cin >> n >> m >> v;
+
+	vector<vector<int>> adj(n + 1);
+
+	while (m--)
+	{
+		int n1, n2;
+		cin >> n1 >> n2;
+		adj[n1].push_back(n2);
+		adj[n2].push_back(n1);
 	}
-}
 
-void dfs(int v) {
-	visited[v] = true;
-	printf("%d ", v);
-	for (auto& elem : nodes[v]) {
-		if (visited[elem] == false) {
-			dfs(elem);
+	deque<bool> visited(n + 1);
+
+	for (auto& elem : adj)
+		sort(elem.begin(), elem.end());
+
+	function<void(int)> DFS = [&](int start)
+	{
+		visited[start] = true;
+		cout << start << ' ';
+
+		for (auto& elem : adj[start])
+		{
+			if (visited[elem])
+				continue;
+			DFS(elem);
 		}
-	}
-}
+	};
+	DFS(v);
 
-void bfs(int v) {
-	visited[v] = true;
-	queue<int> q;
-	q.push(v);
-	while (false == q.empty()) {
-		int w = q.front();
-		q.pop();
-		printf("%d ", w);
-		for (auto& elem : nodes[w]) {
-			if (visited[elem] == false) {
+	cout << '\n';
+	fill(visited.begin(), visited.end(), false);
+
+	auto BFS = [&]()
+	{
+		queue<int> q;
+		q.push(v);
+
+		while (false == q.empty())
+		{
+			int cur = q.front();
+			q.pop();
+			visited[v] = true;
+			cout << cur << ' ';
+
+			for (auto& elem : adj[cur])
+			{
+				if (visited[elem])
+					continue;
 				visited[elem] = true;
 				q.push(elem);
 			}
 		}
-	}
-	
-}
-
-int main() {
-	int n, m, v;
-	cin >> n >> m >> v;
-	visited.resize(n + 1);
-	nodes.resize(n + 1);
-	for (int i = 1; i <= m; i++) {
-		int n1, n2;
-		cin >> n1 >> n2;
-		nodes[n1].push_back(n2);
-		nodes[n2].push_back(n1);
-	}
-	for (auto& elem : nodes) {
-		sort(elem.begin(), elem.end());
-	}
-	dfs(v);
-	initVisited();
-	puts("");
-	bfs(v);
+	};
+	BFS();
 
 	return 0;
 }
