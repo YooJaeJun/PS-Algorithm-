799490cb019132556507b21bd5ae0e1529ba3076
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-05-10 15:46:13 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 136 ms, Memory: 25140 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,39 +1,37 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <string>
-#include <map>
+#include <unordered_map>
 using namespace std;
 
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-
-void solution() {
 	int n, m;
 	cin >> n >> m;
-	map<int, string> mp1;
-	map<string, int> mp2;
-	for (int i = 0; i != n; i++) {
-		string s;
-		cin >> s;
-		mp1[i] = s;
-		mp2[s] = i;
-	}
-	string quest;
-	for (int i = 0; i != m; i++) {
-		cin >> quest;
-		bool isIdx = false;
-		if ((quest.front() >= '0' && quest.front() <= '9') ||
-			(quest.back() >= '0' && quest.back() <= '9')) {
-			cout << mp1[stoi(quest) - 1] << '\n';
-		}
-		else {
-			cout << mp2[quest] + 1 << '\n';
-		}
-	}
-}
 
-int main() {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	solution();
+	unordered_map<string, int> dic1;
+	unordered_map<int, string> dic2;
+	string input;
+	
+	for (int i=1; i<=n; i++)
+	{
+		cin >> input;
+		dic1[input] = i;
+		dic2[i] = input;
+	}
+
+	while (m--)
+	{
+		cin >> input;
+		if (input[0] >= 'A' && input[0] <= 'Z')
+			cout << dic1[input] << '\n';
+		else
+		{
+			int n = stoi(input);
+			cout << dic2[n] << '\n';
+		}
+	}
+
 	return 0;
 }
