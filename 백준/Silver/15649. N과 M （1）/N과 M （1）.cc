bf99d7dc27af839f623b75173e20ab1566535493
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-05-11 11:15:31 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,55 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <deque>
+#include <functional>
 using namespace std;
 
-vector<vector<int>> nodes;
-vector<bool> visited;
-int m;
-
-void dfs(int start, int depth, vector<int>& v) {
-	if (depth == m) {
-		for (auto& elem : v) {
-			cout << elem << ' ';
-		}
-		cout << '\n';
-		return;
-	}
-
-	visited[start] = true;
-
-	for (auto& elem : nodes[start]) {
-		if (visited[elem] == false) {
-			v.push_back(elem);
-			dfs(elem, depth + 1, v);
-			v.pop_back();
-		}
-	}
-
-	visited[start] = false;
-}
-
-void solution() {
-	int n;
-	cin >> n >> m;
-	nodes.resize(n + 1);
-	visited.resize(n + 1);
-
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (i != j) {
-				nodes[i].push_back(j);
-			}
-		}
-	}
-	for (int i = 1; i != nodes.size(); i++) {
-		vector<int> v{i};
-		fill(visited.begin(), visited.end(), false);
-		dfs(i, 1, v);
-	}
-}
-
-int main() {
+int main()
+{
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	solution();
+
+	int n, m;
+	cin >> n >> m;
+	deque<bool> visited(n + 1);
+
+	function<void(vector<int>&, int)> BackTracking = [&](vector<int>& cur, int depth)
+	{
+		if (depth == m)
+		{
+			for (auto& elem : cur)
+				cout << elem << ' ';
+			cout << '\n';
+			return;
+		}
+
+		for (int i=0; i<n; i++)
+		{
+			if (visited[i])
+				continue;
+			visited[i] = true;
+			cur.emplace_back(i + 1);
+			BackTracking(cur, depth + 1);
+			cur.pop_back();
+			visited[i] = false;
+		}
+	};
+	vector<int> cur;
+	BackTracking(cur, 0);
+
 	return 0;
 }
