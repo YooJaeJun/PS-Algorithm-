3b35ca4181cfa627aaf9ff3728f4fda474375e22
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-05-17 11:31:31 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 8 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -1,45 +1,41 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <array>
+#include <vector>
+
 using namespace std;
-#define int int64_t
-using ll = long long;
-using pii = pair<int, int>;
-using vi = vector<int>;
-using vvi = vector<vector<int>>;
-using vvvi = vector<vector<vector<int>>>;
-#define yes cout << "YES\n";
-#define no cout << "NO\n";
-const int maxn = 100'001;
-const double mod = 1e9 + 7;
 
-vvvi dp;
+int dp[21][21][21] = {0,};
 
-int w(int a, int b, int c)
+int DP(int a, int b, int c)
 {
-	if (a <= 0 || b <= 0 || c <= 0) return 1;
-	if (a > 20 || b > 20 || c > 20) return w(20, 20, 20);
-	if (dp[a][b][c] != 0) return dp[a][b][c];
-	if (a < b && b < c) return dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
-	return dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+	if (a <= 0 || b <= 0 || c <= 0)
+		return 1;
+	if (a > 20 || b > 20 || c > 20)
+		return dp[20][20][20];
+	if (dp[a][b][c])
+		return dp[a][b][c];
+	if (a < b && b < c)
+		return dp[a][b][c] = DP(a, b, c - 1) + DP(a, b - 1, c - 1) - DP(a, b - 1, c);
+	return dp[a][b][c] = DP(a - 1, b, c) + DP(a - 1, b - 1, c) + DP(a - 1, b, c - 1) - DP(a - 1, b - 1, c - 1);
 }
 
-void solution()
-{
-	int a, b, c;
-	dp = vvvi(51, vvi(51, vi(51)));
-	w(20, 20, 20);
-	while (1)
-	{
-		cin >> a >> b >> c;
-		if (a == -1 && b == -1 && c == -1) return;
-		cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << '\n';
-	}
-}
-
-int32_t main()
+int main()
 {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int t = 1;
-	// cin >> t;
-	for (int i = 0; i != t; i++) solution();
+
+	DP(20, 20, 20);
+
+	while (true)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+		if (a == -1 && b == -1 && c == -1)
+			return 0;
+
+		int maxn = max(a, max(b, c));
+		int answer = DP(a, b, c);
+		cout << "w(" << a << ", " << b << ", " << c << ") = " << answer << '\n';
+	}
+
 	return 0;
 }
