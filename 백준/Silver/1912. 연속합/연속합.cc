d7cee62d21390fe71fb16599a91bbc4cd0ebb1c8
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-05-18 14:51:02 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,33 +1,31 @@
 #include <iostream>
+#include <array>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
 
-const int inf = -99999999;
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-void solution() {
 	int n;
 	cin >> n;
-	vector<int> dp(n + 1, inf);
-	vector<int> nodes(n + 1);
-	for (int i = 1; i <= n; i++) {
-		cin >> nodes[i];
-		dp[i] = nodes[i];
+
+	vector<int> v(n + 1);
+	vector<int> dp(n + 1);
+
+	for (int i = 1; i <= n; i++)
+		cin >> v[i];
+
+	int answer = dp[1] = v[1];
+
+	for (int i = 2; i <= n; i++)
+	{
+		dp[i] = max(dp[i - 1] + v[i], v[i]);
+		answer = max(answer, dp[i]);
 	}
 
-	int ans = inf;
-	for (int i = 1; i <= n; i++) {
-		dp[i] = max(dp[i], dp[i - 1] + nodes[i]);
-		ans = max(ans, dp[i]);
-	}
+	cout << answer;
 
-	cout << ans;
-}
-
-int main() {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int testCaseNum = 1;
-	// cin >> testCaseNum;
-	for (int i = 0; i != testCaseNum; i++) { solution(); }
 	return 0;
 }
