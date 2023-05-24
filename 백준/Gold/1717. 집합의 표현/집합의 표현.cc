53eaf5f80c3ed04451a64269575e24a36517f672
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-05-24 11:45:28 +0900

    [Gold V] Title: 집합의 표현, Time: 36 ms, Memory: 6328 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -1,60 +1,58 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-#define int int64_t
-using ll = long long;
-using pii = pair<int, int>;
-using vi = vector<int>;
-using vvi = vector<vector<int>>;
 
-vi parent;
+vector<int> parent;
 
-int getParent(int x) {
-	if (x == parent[x]) return x;
-	return parent[x] = getParent(parent[x]);
+int FindParent(int a)
+{
+	if (parent[a] == a)
+		return a;
+	return parent[a] = FindParent(parent[a]);
 }
 
-void unionParent(int a, int b) {
-	a = getParent(a);
-	b = getParent(b);
-	if (a < b) parent[b] = a;
-	else parent[a] = b;
+void Union(int a, int b)
+{
+	a = FindParent(a);
+	b = FindParent(b);
+
+	if (a < b)
+		parent[b] = a;
+	else
+		parent[a] = b;
 }
 
-bool findParent(int a, int b) {
-	a = getParent(a);
-	b = getParent(b);
+bool IsUnion(int a, int b)
+{
+	a = FindParent(a);
+	b = FindParent(b);
 	return a == b;
 }
 
-void solution() {
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
 	int n, m;
 	cin >> n >> m;
-	parent = vi(n + 1);
-	for (int i = 1; i <= n; i++) {
+	parent.resize(n + 1);
+	for (int i = 1; i <= n; i++)
 		parent[i] = i;
+
+	bool q = false;
+	int a = 0, b = 0;
+	string answer[2]{ "NO", "YES" };
+
+	while (m--)
+	{
+		cin >> q >> a >> b;
+
+		if (q)
+			cout << answer[IsUnion(a, b)] << '\n';
+		else
+			Union(a, b);
 	}
 
-	for (int i = 0; i != m; i++) {
-		int question, a, b;
-		cin >> question >> a >> b;
-		if (question == 0) {
-			unionParent(a, b);
-		}
-		else {
-			if (findParent(a, b)) {
-				cout << "YES" << '\n';
-			}
-			else {
-				cout << "NO" << '\n';
-			}
-		}
-	}
-}
 
-int32_t main() {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int t = 1;
-	// cin >> t;
-	for (int i = 0; i != t; i++) { solution(); }
 	return 0;
 }
