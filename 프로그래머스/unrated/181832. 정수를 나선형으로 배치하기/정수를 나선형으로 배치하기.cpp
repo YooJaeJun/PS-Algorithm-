c61183d23d6bf904baccc508347cc05296e5d9df
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-06-06 23:44:35 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.32 ms, Memory: 3.75 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.cpp
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.cpp
@@ -8,36 +8,19 @@ vector<vector<int>> solution(int n) {
     int r = 0, c = 0, cnt = 1;
     answer[0][0] = cnt++;
     
-    while (1)
+    while (cnt <= n * n)
     {
-        bool check = false;
-        
         while (c + 1 < n && answer[r][c + 1] == 0)
-        {
             answer[r][++c] = cnt++;
-            check = true;
-        }   
         
         while (r + 1 < n && answer[r + 1][c] == 0)
-        {
             answer[++r][c] = cnt++;
-            check = true;
-        }
         
         while (c - 1 >= 0 && answer[r][c - 1] == 0)
-        {
             answer[r][--c] = cnt++;
-            check = true;
-        }
         
         while (r - 1 >= 0 && answer[r - 1][c] == 0)
-        {;
             answer[--r][c] = cnt++;
-            check = true;
-        }
-        
-        if (false == check)
-            break;
     }
     
     return answer;
