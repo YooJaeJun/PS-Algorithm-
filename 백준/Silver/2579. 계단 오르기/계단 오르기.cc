66c0501146c059b8b9f907b614ec3dc892935f7b
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-06-09 15:47:30 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,35 +1,30 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-#define int int64_t
-using ll = long long;
-using pii = pair<int, int>;
-using vi = vector<int>;
-using vvi = vector<vector<int>>;
 
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-void solution() {
 	int n;
 	cin >> n;
-	vi arr(n), dp(n);
-	for (int i = 0; i != n; i++) {
-		cin >> arr[i];
-	}
+	vector<int> v(n);
 
-	dp[0] = arr[0];
-	if (arr.size() > 1) dp[1] = arr[0] + arr[1];
-	if (arr.size() > 2) dp[2] = max(arr[0], arr[1]) + arr[2];
+	for (int i = 0; i < n; i++)
+		cin >> v[i];
 
-	for (int i = 3; i < n; i++) {
-		dp[i] = max(dp[i - 3] + arr[i - 1], dp[i - 2]) + arr[i];
-	}
+	vector<int> dp(n);
+	dp[0] = v[0];
+
+	if (n > 0)
+		dp[1] = v[0] + v[1];
+	if (n > 1)
+		dp[2] = max(v[0], v[1]) + v[2];
+
+	for (int i=3; i<n; i++)
+		dp[i] = max(dp[i - 2], dp[i - 3] + v[i - 1]) + v[i];
 
 	cout << dp[n - 1];
-}
 
-int32_t main() {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int t = 1;
-	// cin >> t;
-	for (int i = 0; i != t; i++) { solution(); }
 	return 0;
 }
