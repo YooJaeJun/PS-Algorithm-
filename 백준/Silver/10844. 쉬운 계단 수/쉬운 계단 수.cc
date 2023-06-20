20192a6aabdf4b7ca5a6acc45aa1420a3ec2d986
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-06-20 15:43:03 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -1,56 +1,45 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-#define int int64_t
-using ll = long long;
-using pii = pair<int, int>;
-using vi = vector<int>;
-using vvi = vector<vector<int>>;
-using db = deque<bool>;
-using ddb = deque<deque<bool>>;
-#define forn(i, n) for (int i = 0; i < (int)n; i++)
-#define forn1(i, n) for (int i = 1; i <= (int)n; i++)
-#define inGrid() ((nr >= 0) and (nr < n) and (nc >= 0) and (nc < m))
-int dr[4] = { 0,0,-1,1 };
-int dc[4] = { -1,1,0,0 };	// 좌우상하
 
-
-void solution()
+int main()
 {
-	int mod = 1e9;
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
 	int n;
 	cin >> n;
-	vvi dp(n + 1, vi(10));
-	
-	forn(i, 10)
-	{
-		dp[1][i] = 1;
-	}
-	dp[1][0] = 0;
 
-	for (int i = 2; i <= n; i++)
+	constexpr int inf = 1'000'000'000;
+
+	vector<vector<int>> dp(n + 1, vector<int>(10));	// 자리수, 수
+
+	for (int i = 1; i < 10; i++)
+		dp[1][i] = 1;
+
+	for (int i=2; i<=n; i++)
 	{
-		forn(j, 10)
+		for (int j=0; j<10; j++)
 		{
-			if (j == 0) dp[i][j] = dp[i - 1][j + 1];
-			else if (j == 9) dp[i][j] = dp[i - 1][j - 1];
-			else dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
-			dp[i][j] %= mod;
+			if (j > 0 && j < 9)
+				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % inf;
+			else if (j == 0)
+				dp[i][j] = dp[i - 1][j + 1];
+			else if (j == 9)
+				dp[i][j] = dp[i - 1][j - 1];
+
+			dp[i][j] %= inf;
 		}
 	}
 
 	int ans = 0;
-	forn(i, 10)
+
+	for (int i = 0; i < 10; i++)
 	{
 		ans += dp[n][i];
+		ans %= inf;
 	}
-	cout << ans % mod;
-}
 
-int32_t main()
-{
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int t = 1;
-	// cin >> t;
-	while (t--) solution();
+	cout << ans;
+
 	return 0;
 }
