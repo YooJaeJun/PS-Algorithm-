a8c285a2599b73c584ce74a8fa6ef9d23f8f3a89
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-06-21 13:07:16 +0900

    [Silver I] Title: 회의실 배정, Time: 24 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -3,34 +3,36 @@
 #include <algorithm>
 using namespace std;
 
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-bool comp(const pair<int, int>& p1, const pair<int, int>& p2) {
-	if (p1.second == p2.second) { return p1.first < p2.first; }
-	else { return p1.second < p2.second; }
-}
-
-void solution() {
 	int n;
 	cin >> n;
-	vector<pair<int, int>> nodes(n);
-	for (int i = 0; i != n; i++) {
-		cin >> nodes[i].first >> nodes[i].second;
-	}
+	vector<pair<int, int>> v(n);
 
-	sort(nodes.begin(), nodes.end(), comp);
+	for (int i=0; i<n; i++)
+		cin >> v[i].first >> v[i].second;
 
-	int endTime = 0, cnt = 0;
-	for (int i = 0; i != n; i++) {
-		if (endTime <= nodes[i].first) {
-			endTime = nodes[i].second;
-			cnt++;
+	sort(v.begin(), v.end(), [](pair<int, int> p1, pair<int, int> p2)
+	{
+		if (p1.second == p2.second)
+			return p1.first < p2.first;
+		return p1.second < p2.second;
+	});
+
+	int endTime = 0, ans = 0;
+
+	for (int i=0; i<n; i++)
+	{
+		if (endTime <= v[i].first)
+		{
+			endTime = v[i].second;
+			ans++;
 		}
 	}
-	cout << cnt;
-}
 
-int main() {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	solution();
+	cout << ans;
+	
 	return 0;
 }
