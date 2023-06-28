2e77e72b219cf6b2b24a4fb846aad397edaf924a
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-06-28 15:46:08 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.cc
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.cc
@@ -1,30 +1,40 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int main() {
-	int t;
-	cin >> t;
-	float n;
-	for (int i = 0; i != t; i++) {
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int c;
+	cin >> c;
+	vector<int> v;
+
+	cout << fixed;
+	cout.precision(3);
+
+	while (c--)
+	{
+		int n;
 		cin >> n;
-		vector<int> scores;
+		v.resize(n);
 		float sum = 0;
-		float input;
-		for (int j = 0; j != n; j++) {
-			cin >> input;
-			scores.push_back(input);
-			sum += input;
+
+		for (int i=0; i<n; i++)
+		{
+			cin >> v[i];
+			sum += (float)v[i];
 		}
-		float av = sum / n;
-		float idx = 0;
-		for (const auto& elem : scores) {
-			if (elem > av) {
-				idx++;
-			}
-		}
-		printf("%.3f\%\n", idx / n * 100);
+
+		int average = sum / (float)n;
+		float cnt = 0;
+
+		for (int i=0; i<n; i++)
+			if (average < v[i])
+				cnt++;
+
+		cout << cnt / n * 100.0f << "%" << '\n';
 	}
+
 	return 0;
 }
