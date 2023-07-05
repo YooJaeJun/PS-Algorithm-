351d481905eccc13844a00d379d15d969bf20573
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-07-05 16:01:18 +0900

    [Silver II] Title: 종이의 개수, Time: 376 ms, Memory: 20836 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.cc
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -1,69 +1,52 @@
 #include <iostream>
-#include <vector>
+#include <deque>
+#include <functional>
 using namespace std;
 
-vector<vector<int>> v;
-int zeroCnt = 0, minusOneCnt = 0, oneCnt = 0;
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-void plusCnt(int num) {
-	switch (num) {
-	case -1:
-		minusOneCnt++;	break;
-	case 0:
-		zeroCnt++;	break;
-	case 1:
-		oneCnt++;	break;
-	}
-}
-
-void div(int x, int y, int size) {
-	if (size == 1) {
-		plusCnt(v[x][y]);
-		return;
-	}
-
-	int num = v[x][y];
-	bool possible = true;
-	for (int i = x; i != x + size; i++) {
-		for (int j = y; j != y + size; j++) {
-			if (num != v[i][j]) {
-				possible = false;
-			}
-		}
-	}
-
-	if (possible == false) {
-		for (int i = 0; i < size; i += size / 3) {
-			for (int j = 0; j < size; j += size / 3) {
-				div(x + i, y + j, size / 3);
-			}
-		}
-	}
-	else {
-		plusCnt(num);
-	}
-
-}
-
-void solution() {
 	int n;
 	cin >> n;
-	v.resize(n);
-	for (auto& elem : v) {
-		elem.resize(n);
-	}
-	for (int i = 0; i != n; i++) {
-		for (int j = 0; j != n; j++) {
-			cin >> v[i][j];
+	vector<vector<int>> grid(n, vector<int>(n));
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			cin >> grid[i][j];
+
+	vector<int> ans(3);
+
+	function<void(int, int, int)> DivideConquer = [&](int size, int x, int y)
+	{
+		if (size == 1)
+		{
+			ans[grid[x][y] + 1]++;
+			return;
 		}
-	}
 
-	div(0, 0, n);
-	cout << minusOneCnt << '\n' << zeroCnt << '\n' << oneCnt;
-}
+		bool loopBreak = false;
 
-int main() {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	solution();
+		for (int r = x; r < x + size && !loopBreak; r++)
+			for (int c = y; c < y + size && !loopBreak; c++)
+				if (grid[r][c] != grid[x][y])
+					loopBreak = true;
+
+		if (loopBreak)
+		{
+			for (int i = 0; i < 3; i++)
+				for (int j = 0; j < 3; j++)
+					DivideConquer(size / 3, 
+						x + size / 3 * i, 
+						y + size / 3 * j);
+		}
+		else
+			ans[grid[x][y] + 1]++;
+	};
+	DivideConquer(n, 0, 0);
+
+	for (auto& elem : ans)
+		cout << elem << '\n';
+
 	return 0;
 }
