4a99db4de60330f3adfb95ccb63a4be1cc4a79a5
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2023-08-01 23:35:12 +0900

    [Gold V] Title: 평범한 배낭, Time: 44 ms, Memory: 42004 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -2,34 +2,31 @@
 #include <vector>
 using namespace std;
 
-void solution() {
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
 	int n, k;
 	cin >> n >> k;
-	vector<vector<int>> dp(n + 1, vector<int>(k + 1));
-	vector<int> w(n + 1);
-	vector<int> v(n + 1);
-	for (int i = 1; i <= n; i++) {
-		cin >> w[i] >> v[i];
-	}
+	vector<int> v(n + 1), w(n + 1);
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= k; j++) {
-			if (j - w[i] >= 0) {
+	for (int i = 1; i <= n; i++)
+		cin >> w[i] >> v[i];
+
+	vector<vector<int>> dp(n + 1, vector<int>(k + 1));
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= k; j++)
+		{
+			if (j >= w[i])
 				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
-			}
-			else {
+			else
 				dp[i][j] = dp[i - 1][j];
-			}
 		}
 	}
 
 	cout << dp[n][k];
-}
 
-int main() {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int testCaseNum = 1;
-	// cin >> testCaseNum;
-	for (int i = 0; i != testCaseNum; i++) { solution(); }
 	return 0;
 }
